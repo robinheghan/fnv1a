9f8612383346b8b9e4ae005ffdc26c40a04f6e60
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2021-10-29 14:30:05 +0200

    Implement hash algo.

--- src/FNV1a.elm
+++ src/FNV1a.elm
@@ -1,11 +1,36 @@
 module FNV1a exposing (hash, hashWithSeed)
 
+import Bitwise
+
+
+
+{- Implementation has been ported from here: https://gist.github.com/vaiorabbit/5657561 -}
+
+
+initialSeed : Int
+initialSeed =
+    0x811C9DC5
+
 
 hash : String -> Int
 hash str =
-    0
+    hashWithSeed initialSeed str
 
 
 hashWithSeed : Int -> String -> Int
 hashWithSeed seed str =
-    0
+    Bitwise.shiftRightZfBy 0 (String.foldl hasher seed str)
+
+
+hasher : Char -> Int -> Int
+hasher char hashValue =
+    let
+        withCharCode =
+            Bitwise.xor (Char.toCode char) hashValue
+    in
+    withCharCode
+        + Bitwise.shiftLeftBy 1 withCharCode
+        + Bitwise.shiftLeftBy 4 withCharCode
+        + Bitwise.shiftLeftBy 7 withCharCode
+        + Bitwise.shiftLeftBy 8 withCharCode
+        + Bitwise.shiftLeftBy 24 withCharCode
